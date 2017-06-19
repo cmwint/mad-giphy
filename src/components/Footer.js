ffcc481c1f5cbedb687bdc84b012e4ba90f77ac5
Corinne Winthrop <corinne.winthrop@gmail.com>
2017-06-18 23:47:53 -0600

    Remove errors about rel link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
 			<div className="footer__wrap">
 				<div className="footer__copyright">© 2017 Corinne Winthrop</div>
 				<div className="footer__site">
-					<a href="http://corinnewinthrop.com/" target="_blank">Check out my website!</a>
+					<a href="http://corinnewinthrop.com/" rel="noopener noreferrer" target="_blank">Check out my website!</a>
 				</div>
 			</div>
 		</div>
